fe65938b3c1931552f1bbbc6ef3995ceba2ed3e0
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-03 09:21:18 -0300

    Torna cache PWA da v9 mais robusto

--- tangram-prof-junior-v9/sw.js
+++ tangram-prof-junior-v9/sw.js
@@ -1,5 +1,25 @@
-const CACHE='tangram-rai-v9-1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.svg','./icon-512.svg','../tangram-prof-junior/rai-chalk.webp','../tangram-prof-junior/chunk01.txt','../tangram-prof-junior/chunk02-03.txt','../tangram-prof-junior/chunk04-05.txt','../tangram-prof-junior/chunk06-07.txt','../tangram-prof-junior/chunk08-09.txt'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request,{ignoreSearch:true}).then(cached=>{if(cached)return cached;return fetch(event.request).then(resp=>{if(resp&&resp.ok&&new URL(event.request.url).origin===self.location.origin){const clone=resp.clone();caches.open(CACHE).then(c=>c.put(event.request,clone))}return resp}).catch(()=>caches.match('./index.html'))}))});
+const CACHE='tangram-rai-v9-3';
+const ASSETS=[
+  './','./index.html','./manifest.webmanifest','./v9.css','./app-v9.js','./icon-192.png','./icon-512.png','./apple-touch-icon.png',
+  '../tangram-prof-junior/rai-chalk.webp','../tangram-prof-junior/chunk01.txt','../tangram-prof-junior/chunk02-03.txt','../tangram-prof-junior/chunk04-05.txt','../tangram-prof-junior/chunk06-07.txt','../tangram-prof-junior/chunk08-09.txt'
+];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>Promise.all(ASSETS.map(u=>cache.add(u).catch(()=>null)))).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(resp=>{
+      if(resp&&resp.ok){const clone=resp.clone();caches.open(CACHE).then(c=>c.put('./index.html',clone));}
+      return resp;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request,{ignoreSearch:true}).then(cached=>cached||fetch(event.request).then(resp=>{
+    if(resp&&resp.ok&&new URL(event.request.url).origin===self.location.origin){const clone=resp.clone();caches.open(CACHE).then(c=>c.put(event.request,clone));}
+    return resp;
+  })));
+});
