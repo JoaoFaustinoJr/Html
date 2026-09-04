1b15a8805283977cbf0456d541a8dd0d30402588
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-04 09:24:30 -0300

    Atualiza ícone Maker Quest e card de compartilhamento

--- maker-quest/sw.js
+++ maker-quest/sw.js
@@ -1,5 +1,5 @@
-const CACHE='maker-quest-v1-20260904';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./rai-icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+const CACHE='maker-quest-v2-20260904';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./rai-icon.svg','./icon-192.png','./icon-512.png','./maker-quest-share.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(x=>x.put(e.request,cp));return r}).catch(()=>caches.match('./index.html'))))});
