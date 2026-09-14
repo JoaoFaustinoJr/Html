bf703e319757e83b38369762bc42895e32f4368f
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-14 14:41:41 -0300

    Atualiza cache do modo Jovem para v45

--- tia-tati-jovem/sw.js
+++ tia-tati-jovem/sw.js
@@ -1,4 +1,4 @@
-const CACHE='tia-tati-jovem-pwa-v44';
+const CACHE='tia-tati-jovem-pwa-v45';
 self.addEventListener('install',e=>{e.waitUntil(self.skipWaiting())});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('tia-tati-jovem-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
