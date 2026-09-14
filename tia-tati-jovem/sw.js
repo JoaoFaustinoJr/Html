e3ff2b81a3762138a45321a44088227ad952423c
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-14 17:01:53 -0300

    chore(tia-tati-jovem): alinhar service worker com base segura v58

--- tia-tati-jovem/sw.js
+++ tia-tati-jovem/sw.js
@@ -1,4 +1,4 @@
-const VERSION='tia-tati-jovem-sw-v57';
+const VERSION='tia-tati-jovem-sw-v58-safe';
 self.addEventListener('install',e=>{e.waitUntil(self.skipWaiting())});
 self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('tia-tati-jovem-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
