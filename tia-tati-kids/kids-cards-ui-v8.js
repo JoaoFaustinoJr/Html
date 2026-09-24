b35f6c617e8cc04e100d46253308b4074ebfc039
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-24 13:23:04 -0300

    Tia Tati Kids: prepara cards aprovados v14

--- tia-tati-kids/kids-cards-ui-v8.js
+++ tia-tati-kids/kids-cards-ui-v8.js
@@ -15,9 +15,9 @@ const CARDS=[
  {id:'preciso-ajuda',title:'Preciso de ajuda',subtitle:'Tudo bem pedir ajuda quando precisar.',sprite:'assets/cards/cards-pair-5.webp',side:1},
  {id:'muito-bem',title:'Muito bem!',subtitle:'Seu esforço merece ser reconhecido.',sprite:'assets/cards/cards-pair-6.webp',side:0},
  {id:'amizade',title:'Amizade também move o mundo!',subtitle:'Inclusão, respeito e amizade.',sprite:'assets/cards/cards-pair-6.webp',side:1},
- {id:'carro-tia-tati',title:'Carro da Tia Tati',subtitle:'Vamos juntos!',image:'assets/cards/carro-tia-tati-v1.webp'},
- {id:'abelhinha',title:'Abelhinha',subtitle:'Você consegue!',image:'assets/bee-garden.svg'},
- {id:'espelho-magico',title:'Espelho Mágico',subtitle:'Veja como você é especial!',image:'../tia-tati-espelho/assets/mask-v6-2.webp'}
+ {id:'carro-tia-tati',title:'Carro da Tia Tati',subtitle:'Vamos juntos!',image:'assets/cards/carro-tia-tati-v14.png'},
+ {id:'abelhinha',title:'Abelhinha',subtitle:'Você consegue!',image:'assets/cards/abelhinha-tia-tati-v14.png'},
+ {id:'espelho-magico',title:'Espelho Mágico',subtitle:'Veja como você é especial!',image:'assets/cards/espelho-magico-tia-tati-v14.png'}
 ];
 const q=(s,r=document)=>r.querySelector(s),qa=(s,r=document)=>[...r.querySelectorAll(s)];
 window.TiaTatiKidsCardsData=CARDS;
